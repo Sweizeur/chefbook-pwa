24a75aedfd0aef3e01703f32fb6dd75835f47c99
Anthony Ong <anthony.ong@aj-innov.fr>
2025-09-30 12:09:08 +0200

    Deploy PWA to GitHub Pages

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'chefbook-v1';
+const urlsToCache = [
+  '/',
+  '/_expo/static/js/web/index-72b8e7d22565edb680c689056efbcdab.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
